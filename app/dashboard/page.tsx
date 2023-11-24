b90cf032b33bbeb74fa1d0e13729e1f7032a27d6
Lenin Santiago <lnnsntg@gmail.com>
2023-11-24 14:54:49 +0100

    Inital commit

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+
+export default function Page() {
+    return (
+        <div>Dashboard Page nested into Layout component created in dashboard, the page holded
+            in dashboard folder be show here into de layout component</div>
+    )
+}
---
+++ app/ui/fonts.ts
@@ -0,0 +1,4 @@
+import { Inter, Lusitana, Raleway } from 'next/font/google'
+export const lusitana = Lusitana({ subsets: ['latin'], weight: ['400', '700'] })
+export const inter = Inter({ subsets: ['latin'] })
+export const raleway = Raleway({ subsets: ['latin'], weight: ['400', '500'] })
